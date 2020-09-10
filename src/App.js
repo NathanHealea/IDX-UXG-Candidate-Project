3c3e0237bcc6b94c227b8927d2beb4f03cbb4a6b
Nathan Healea <nate.healea@gmail.com>
2020-09-10 01:17:42 -0700

    Remove css and test.js from project; WIP - Listing Api implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// --- Package Imports --- //
+import React, { useEffect } from 'react';
 
+// --- Redux --- //
+import { useDispatch, useSelector } from 'react-redux';
+
+// --- Store --- //
+import {
+  types as ListingTypes,
+  operations as ListingOperations,
+} from 'store/Listing';
+
+/**
+ * Main component for application
+ */
 function App() {
+  const listing = useSelector((store) => store.listing);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (listing.status === ListingTypes.STATUS_INIT) {
+      dispatch(ListingOperations.getAllListings());
+    }
+  }, [dispatch, listing.status]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {listing.list.map((house) => (
+        <p>{JSON.stringify(house)}</p>
+      ))}
     </div>
   );
 }
