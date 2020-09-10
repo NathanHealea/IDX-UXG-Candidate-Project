8ce4beb56ce4581a81c74d9aa6367baaf10322e0
Nathan Healea <nate.healea@gmail.com>
2020-09-10 01:20:31 -0700

    Setup Redux provider for application; Remove logo.svg from project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+// --- Imports --- //
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// --- Store Imports --- //
+import { Provider } from 'react-redux';
+import store from './store';
+
+// --- Custom Component Import --- //
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
